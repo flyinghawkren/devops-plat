2a851d76f9e076670c9bc92abdfc6bd1debbcb3d
10033385 <gong.mingsheng@zte.com.cn>
2019-08-13 17:26:35 +0800

    compile error

--- demo/java/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/java/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/", method = GET)
+    @RequestMapping(value="/", method=RequestMethod.GET)
     public String hello(){
         String str = "Hello World! v1.0.0";
         return str;

558d00460cd60669c79315641b3d75408fb064a2
10033385 <gong.mingsheng@zte.com.cn>
2019-08-19 01:23:43 +0800

    compile error

--- demo/cpp/main.cpp
+++ demo/cpp/main.cpp
@@ -1,13 +1,11 @@
-#include "inc/MinMax.h"
-
 #include <pistache/endpoint.h>
+#include "inc/MinMax.h"
 
 using namespace Pistache;
 
 struct HelloHandler : public Http::Handler {
     HTTP_PROTOTYPE(HelloHandler)
-
-    void onRequest(const Http::Request& req, Http::ResponseWriter writer) override {
+    void onRequest(const Http::Request&, Http::ResponseWriter writer) override {
         writer.send(Http::Code::Ok, "Hello, World! v1.0.0");
     }
 };
